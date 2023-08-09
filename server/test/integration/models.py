ec20a8b949f4789f789d77da6b52c1d358f3e86a
Ruben Perez <rubenperez038@gmail.com>
2023-08-09 13:16:17 +0200

    pydantic models

---
+++ server/test/integration/models.py
@@ -0,0 +1,53 @@
+# copy
+
+from pydantic import BaseModel, Field
+from typing import List, Literal
+
+class User(BaseModel):
+    id: str
+    username: str
+
+
+class MessageWithoutId(BaseModel):
+    user: User
+    content: str
+
+
+class Message(MessageWithoutId):
+    id: str = Field(pattern=r'[0-9]+\-[0-9]+')
+
+
+class Room(BaseModel):
+    id: str
+    name: str
+    messages: List[Message]
+    hasMoreMessages: bool
+
+
+class HelloEventPayload(BaseModel):
+    rooms: List[Room]
+
+
+class HelloEvent(BaseModel):
+    type: Literal['hello']
+    payload: HelloEventPayload
+
+
+class SentMessagesEventPayload(BaseModel):
+    roomId: str
+    messages: List[MessageWithoutId]
+
+
+class SentMessagesEvent(BaseModel):
+    type: Literal['messages']
+    payload: SentMessagesEventPayload
+
+
+class ReceivedMessagesEventPayload(BaseModel):
+    roomId: str
+    messages: List[Message]
+
+
+class ReceivedMessagesEvent(BaseModel):
+    type: Literal['messages']
+    payload: ReceivedMessagesEventPayload
--- server/test/integration/test_websocket.py
+++ server/test/integration/test_websocket.py
@@ -5,7 +5,7 @@ from websockets.sync.client import connect
 import pytest
 import redis
 from contextlib import contextmanager
-import re
+from .models import HelloEvent, HelloEventPayload, Room, MessageWithoutId, ReceivedMessagesEvent, SentMessagesEvent, SentMessagesEventPayload, User
 
 @pytest.fixture(scope="session", autouse=True)
 def prepare_db(request):
@@ -14,41 +14,40 @@ def prepare_db(request):
 
 def test_hello():
     with connect("ws://localhost:8080") as websocket:
-        message = websocket.recv(timeout=3)
-        expected = {
-            "type": "hello",
-            "payload": {
-                "rooms": [
-                    {
-                        "id": "beast",
-                        "name": "Boost.Beast",
-                        "messages": [],
-                        "hasMoreMessages": False,
-                    },
-                    {
-                        "id": "async",
-                        "name": "Boost.Async",
-                        "messages": [],
-                        "hasMoreMessages": False,
-                    },
-                    {
-                        "id": "db",
-                        "name": "Database connectors",
-                        "messages": [],
-                        "hasMoreMessages": False,
-                    },
-                    {
-                        "id": "wasm",
-                        "name": "Web assembly",
-                        "messages": [],
-                        "hasMoreMessages": False,
-                    },
+        message = HelloEvent.model_validate_json(websocket.recv(timeout=3))
+        expected = HelloEvent(
+            type="hello",
+            payload=HelloEventPayload(
+                rooms=[
+                    Room(
+                        id="beast",
+                        name="Boost.Beast",
+                        messages=[],
+                        hasMoreMessages=False
+                    ),
+                    Room(
+                        id="async",
+                        name="Boost.Async",
+                        messages=[],
+                        hasMoreMessages=False
+                    ),
+                    Room(
+                        id="db",
+                        name="Database connectors",
+                        messages=[],
+                        hasMoreMessages=False
+                    ),
+                    Room(
+                        id="wasm",
+                        name="Web assembly",
+                        messages=[],
+                        hasMoreMessages=False
+                    ),
                 ]
-            }
-        }
-        actual = json.loads(message)
+            )
+        )
 
-        assert actual == expected
+        assert message.model_dump_json() == expected.model_dump_json()
 
 # Clean up any received message before closing the socket.
 # There seems to be a race condition that hangs close if we don't do this.
@@ -67,39 +66,36 @@ def connect_websocket():
 
 
 def test_sending_messages():
-        with connect_websocket() as ws1:
-            with connect_websocket() as ws2:
-                # Read hello messages
-                ws1.recv(timeout=1)
-                ws2.recv(timeout=1)
+    with connect_websocket() as ws1:
+        with connect_websocket() as ws2:
+            # Read hello messages
+            HelloEvent.model_validate_json(ws1.recv(timeout=1))
+            HelloEvent.model_validate_json(ws2.recv(timeout=1))
 
-                # Send a message through ws1
-                msg = {
-                    "roomId": "wasm",
-                    "messages": [{
-                        "user": {
-                            "id": "user1",
-                            "username": "username1"
-                        },
-                        "content": "Test message 1"
-                    }]
-                }
-                ws1.send(json.dumps({ "type": "messages", "payload": msg }))
+            # Send a message through ws1
+            user = User(id="user1", username="username1")
+            sent_msg = SentMessagesEvent(
+                type='messages',
+                payload=SentMessagesEventPayload(
+                    roomId="wasm",
+                    messages=[MessageWithoutId(
+                        user=user,
+                        content="Test message 1"
+                    )]
+                )
+            )
+            ws1.send(sent_msg.model_dump_json())
 
-                # Both ws1 and ws2 should receive it
-                ws1_msg = json.loads(ws1.recv())
-                ws2_msg = json.loads(ws2.recv())
+            # Both ws1 and ws2 should receive it
+            ws1_msg = ReceivedMessagesEvent.model_validate_json(ws1.recv())
+            ws2_msg = ReceivedMessagesEvent.model_validate_json(ws2.recv())
 
-                # Validate one of them
-                assert ws1_msg["type"] == "messages"
-                payload = ws1_msg["payload"]
-                assert payload["roomId"] == "wasm"
-                assert len(payload["messages"]) == 1
-                recv_msg = payload["messages"][0]
-                assert recv_msg["user"] == msg["messages"][0]["user"]
-                assert recv_msg["content"] == "Test message 1"
-                assert re.match(r'[0-9]+\-[0-9]+', recv_msg["id"]) is not None
+            # Validate one of them
+            assert len(ws1_msg.payload.messages) == 1
+            recv_msg = ws1_msg.payload.messages[0]
+            assert recv_msg.user == user
+            assert recv_msg.content == "Test message 1"
 
-                # The other one should be identical
-                assert ws1_msg == ws2_msg
+            # The other one should be identical
+            assert ws1_msg.model_dump_json() == ws2_msg.model_dump_json()
 
