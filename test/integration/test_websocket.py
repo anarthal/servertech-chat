f68e30e0c60c8b59d6458683b224defb12674f84
Ruben Perez <rubenperez038@gmail.com>
2023-08-10 12:02:56 +0200

    Message history test

--- test/integration/test_websocket.py
+++ test/integration/test_websocket.py
@@ -26,7 +26,7 @@ def test_hello():
 @contextmanager
 def connect_websocket():
     try:
-        ws = connect("ws://localhost:8080", close_timeout=1)
+        ws = connect("ws://localhost:8080", close_timeout=0.1)
         yield ws
     finally:
         try:
@@ -37,7 +37,7 @@ def connect_websocket():
         ws.close()
 
 
-def test_sending_messages():
+def test_send_receive_messages():
     with connect_websocket() as ws1:
         with connect_websocket() as ws2:
             # Read hello messages
@@ -70,4 +70,72 @@ def test_sending_messages():
 
             # The other one should be identical
             assert ws1_msg.model_dump_json() == ws2_msg.model_dump_json()
+
+            # Send a message through ws2
+            sent_msg = SentMessagesEvent(
+                type='messages',
+                payload=SentMessagesEventPayload(
+                    roomId="beast",
+                    messages=[MessageWithoutId(
+                        user=user,
+                        content="Test message 2"
+                    )]
+                )
+            )
+            ws2.send(sent_msg.model_dump_json())
+
+            # Both ws1 and ws2 should receive it
+            ws1_msg = ReceivedMessagesEvent.model_validate_json(ws1.recv())
+            ws2_msg = ReceivedMessagesEvent.model_validate_json(ws2.recv())
+
+            # Sanity checks
+            assert ws1_msg.model_dump_json() == ws2_msg.model_dump_json()
+            assert ws1_msg.payload.roomId == 'beast'
+
+
+def test_new_messages_appear_in_history():
+    with connect_websocket() as ws1:
+        # Read hello message
+        hello_1 = HelloEvent.model_validate_json(ws1.recv(timeout=1))
+
+        # Send a couple of messages
+        user = User(id="user1", username="username1")
+        msg_1 = SentMessagesEvent(
+            type='messages',
+            payload=SentMessagesEventPayload(
+                roomId="wasm",
+                messages=[MessageWithoutId(
+                    user=user,
+                    content="Test message 1"
+                )]
+            )
+        )
+        msg_2 = SentMessagesEvent(
+            type='messages',
+            payload=SentMessagesEventPayload(
+                roomId="wasm",
+                messages=[MessageWithoutId(
+                    user=user,
+                    content="Test message 2"
+                )]
+            )
+        )
+        ws1.send(msg_1.model_dump_json())
+        ws1.send(msg_2.model_dump_json())
+
+        # Connect another websocket and read the hello
+        with connect_websocket() as ws2:
+            hello_2 = HelloEvent.model_validate_json(ws2.recv(timeout=1))
+        
+    # Validate that two new messages were added to the room's history
+    wasm_room_1 = next(r for r in hello_1.payload.rooms if r.id == 'wasm')
+    wasm_room_2 = next(r for r in hello_2.payload.rooms if r.id == 'wasm')
+    assert len(wasm_room_2.messages) == len(wasm_room_1.messages) + 2
+
+    # Last message goes first
+    assert wasm_room_2.messages[0].content == 'Test message 2'
+    assert wasm_room_2.messages[1].content == 'Test message 1'
+    assert wasm_room_2.messages[0].id > wasm_room_2.messages[1].id
+
+
 
