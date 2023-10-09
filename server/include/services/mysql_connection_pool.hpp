1c3ea3b33c9ac3be5e02ecb9cad4eecbe274d991
Ruben Perez <rubenperez038@gmail.com>
2023-10-09 12:57:51 +0200

    renamed files

--- server/include/services/mysql_connection_pool.hpp
+++ server/include/services/mysql_connection_pool.hpp
@@ -1,12 +1,12 @@
 //
-// Copyright (c) 2019-2023 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+// Copyright (c) 2023 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef BOOST_MYSQL_CONNECTION_POOL_HPP
-#define BOOST_MYSQL_CONNECTION_POOL_HPP
+#ifndef SERVERTECHCHAT_SERVER_INCLUDE_SERVICES_MYSQL_CONNECTION_POOL_HPP
+#define SERVERTECHCHAT_SERVER_INCLUDE_SERVICES_MYSQL_CONNECTION_POOL_HPP
 
 #include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/spawn.hpp>
--- server/src/services/mysql_connection_pool.cpp
+++ server/src/services/mysql_connection_pool.cpp
@@ -1,11 +1,11 @@
 //
-// Copyright (c) 2019-2023 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+// Copyright (c) 2023 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#include "services/connection_pool.hpp"
+#include "services/mysql_connection_pool.hpp"
 
 #include <boost/asio/any_io_executor.hpp>
 #include <boost/asio/deferred.hpp>
