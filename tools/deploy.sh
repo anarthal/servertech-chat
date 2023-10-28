ac7e846f07f4e7c572d7e8a3727fee7886a26259
Ruben Perez <rubenperez038@gmail.com>
2023-10-28 18:33:52 +0200

    Corrected mysql data dir

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -14,7 +14,7 @@ sudo docker network rm chat-net || echo "Network does not exist"
 
 # Run our application
 sudo docker run -d --name redis -v /data/redis-data:/data redis:alpine redis-server --appendonly yes
-sudo docker run -d --name mysql -v /data/mysql-data:/data -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:8.1.0
+sudo docker run -d --name mysql -v /data/mysql-data:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:8.1.0
 sudo docker run -d -p 80:80 -e REDIS_HOST=redis -e MYSQL_HOST=mysql --name cppserver $1
 sudo docker network create chat-net
 sudo docker network connect chat-net redis
