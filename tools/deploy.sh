b4fb77918c654f5373724936576f303f516de57e
Ruben Perez <rubenperez038@gmail.com>
2023-10-28 17:23:45 +0200

    SSH deploy 4

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -15,10 +15,9 @@ sudo docker network rm chat-net || echo "Network does not exist"
 # Run our application
 sudo docker run -d --name redis -v /data/redis-data:/data redis:alpine redis-server --appendonly yes
 sudo docker run -d --name mysql -v /data/mysql-data:/data -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:8.1.0
-sudo docker run -d -p 80:80 -e REDIS_HOST=redis -e MYSQL_HOST=mysql --name cppserver ghcr.io/anarthal/servertech-chat:latest
+sudo docker run -d -p 80:80 -e REDIS_HOST=redis -e MYSQL_HOST=mysql --name cppserver $1
 sudo docker network create chat-net
 sudo docker network connect chat-net redis
 sudo docker network connect chat-net mysql
 sudo docker network connect chat-net cppserver
 sudo docker image prune -a -f # Remove unused images
-
