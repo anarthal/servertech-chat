22b34bdfb5d0c7c4ba65845e6aefaf6b48cf63bf
Ruben Perez <rubenperez038@gmail.com>
2024-11-11 19:20:29 +0100

    Update mysql_client to use client-side SQL

--- server/src/services/mysql_client.cpp
+++ server/src/services/mysql_client.cpp
@@ -23,11 +23,11 @@
 #include <boost/mysql/results.hpp>
 #include <boost/mysql/static_results.hpp>
 #include <boost/mysql/tcp.hpp>
+#include <boost/mysql/with_params.hpp>
 
 #include <chrono>
 #include <exception>
 #include <memory>
-#include <sstream>
 #include <string>
 #include <string_view>
 
@@ -37,6 +37,7 @@
 #include "error.hpp"
 
 using namespace chat;
+namespace mysql = boost::mysql;
 
 // DB setup code. This is executed once at startup.
 // This should be improved when implementing
@@ -68,12 +69,12 @@ static std::string get_mysql_hostname()
 // Returns the default pool params to use.
 // This should be improved when implementing
 // https://github.com/anarthal/servertech-chat/issues/45
-static boost::mysql::pool_params get_pool_params()
+static mysql::pool_params get_pool_params()
 {
-    boost::mysql::pool_params res{
+    mysql::pool_params res{
         // The server address. We get the hostname fron an environment
         // variable, and use the default port.
-        boost::mysql::host_and_port{get_mysql_hostname()},
+        mysql::host_and_port{get_mysql_hostname()},
 
         // The username to log in as
         "root",
@@ -87,7 +88,7 @@ static boost::mysql::pool_params get_pool_params()
 
     // Since our server will be running in the same node as the web server,
     // we don't need any encryption
-    res.ssl = boost::mysql::ssl_mode::disable;
+    res.ssl = mysql::ssl_mode::disable;
 
     return res;
 }
@@ -96,44 +97,42 @@ namespace {
 
 class mysql_client_impl final : public mysql_client
 {
-    boost::mysql::connection_pool pool_;
+    mysql::connection_pool pool_;
 
     // Functions to retrieve a MySQL connection with retries.
     // This is only used for the connection required by setup_db.
-    result_with_message<boost::mysql::any_connection> get_connection_with_retries(
-        boost::asio::yield_context yield
-    )
+    result_with_message<mysql::any_connection> get_connection_with_retries(boost::asio::yield_context yield)
     {
         // Connection params to use. Hostname, user and password are the same.
         // The database may not be created when we run this, so we leave it blank.
-        boost::mysql::connect_params params{
-            boost::mysql::host_and_port{get_mysql_hostname()},
+        mysql::connect_params params{
+            mysql::host_and_port{get_mysql_hostname()},
             "root",
             "",
             "",
         };
-        params.ssl = boost::mysql::ssl_mode::disable;
+        params.ssl = mysql::ssl_mode::disable;
         params.multi_queries = true;
 
         // We use mysql::any_connection here because it's easier to use than regular mysql::connection.
         // For instance, it manages hostname resolution automatically.
-        boost::mysql::any_connection conn(yield.get_executor());
+        mysql::any_connection conn(yield.get_executor());
         boost::asio::steady_timer timer(yield.get_executor());
-        boost::mysql::diagnostics diag;
+        mysql::diagnostics diag;
 
         error_code ec;
 
         while (true)
         {
             // Try to connect
-            conn.async_connect(&params, diag, yield[ec]);
+            conn.async_connect(params, diag, yield[ec]);
             if (ec)
             {
                 // Log the error
                 log_error({ec, diag.server_message()}, "Failed to connect to mysql");
 
                 // Wait some time
-                timer.expires_from_now(std::chrono::seconds(2));
+                timer.expires_after(std::chrono::seconds(2));
                 timer.async_wait(yield[ec]);
 
                 // If the timer wait errored, it was cancelled, so exit the loop
@@ -154,8 +153,8 @@ public:
     error_with_message setup_db(boost::asio::yield_context yield) final override
     {
         error_code ec;
-        boost::mysql::diagnostics diag;
-        boost::mysql::results result;
+        mysql::diagnostics diag;
+        mysql::results result;
 
         // Get a connection. Perform retries until we get a connection or
         // we get cancelled via Ctrl-C. This provides safety against MySQL
@@ -197,27 +196,29 @@ public:
         boost::asio::yield_context yield
     ) final override
     {
-        boost::mysql::diagnostics diag;
+        mysql::diagnostics diag;
         error_code ec;
-        boost::mysql::results result;
+        mysql::results result;
 
         // Get a connection
         auto conn = pool_.async_get_connection(diag, yield[ec]);
         if (ec)
             return error_with_message{ec, diag.server_message()};
 
-        // Prepare a statement
-        constexpr std::string_view
-            stmt_sql = R"SQL(INSERT INTO users (username, email, password) VALUES (?, ?, ?))SQL";
-        auto stmt = conn->async_prepare_statement(stmt_sql, diag, yield[ec]);
-        if (ec)
-            return error_with_message{ec, diag.server_message()};
-
-        // Execute it
-        conn->async_execute(stmt.bind(username, email, hashed_password), result, diag, yield[ec]);
+        // Execute the insertion
+        conn->async_execute(
+            mysql::with_params(
+                "INSERT INTO users (username, email, password) VALUES ({}, {}, {})",
+                email,
+                hashed_password
+            ),
+            result,
+            diag,
+            yield[ec]
+        );
 
         // Detect duplicates
-        if (ec == boost::mysql::common_server_errc::er_dup_entry)
+        if (ec == mysql::common_server_errc::er_dup_entry)
         {
             // As per MySQL documentation, error messages for er_dup_entry
             // are formatted as: Duplicate entry '%s' for key %d
@@ -241,7 +242,7 @@ public:
     result_with_message<auth_user> get_user_by_email(std::string_view email, boost::asio::yield_context yield)
         final override
     {
-        boost::mysql::diagnostics diag;
+        mysql::diagnostics diag;
         error_code ec;
 
         // Get a connection
@@ -249,17 +250,15 @@ public:
         if (ec)
             return error_with_message{ec, diag.server_message()};
 
-        // Prepare a statement. static_results requires that SQL field names
+        // static_results requires that SQL field names
         // match with C++ struct field names, so we use SQL aliases
-        constexpr std::string_view
-            stmt_sql = R"SQL(SELECT id, password AS hashed_password FROM users WHERE email = ?)SQL";
-        auto stmt = conn->async_prepare_statement(stmt_sql, diag, yield[ec]);
-        if (ec)
-            return error_with_message{ec, diag.server_message()};
-
-        // Execute it
-        boost::mysql::static_results<auth_user> result;
-        conn->async_execute(stmt.bind(email), result, diag, yield[ec]);
+        mysql::static_results<auth_user> result;
+        conn->async_execute(
+            mysql::with_params("SELECT id, password AS hashed_password FROM users WHERE email = {}", email),
+            result,
+            diag,
+            yield[ec]
+        );
         if (ec)
             return error_with_message{ec, diag.server_message()};
 
@@ -274,7 +273,7 @@ public:
     result_with_message<user> get_user_by_id(std::int64_t user_id, boost::asio::yield_context yield)
         final override
     {
-        boost::mysql::diagnostics diag;
+        mysql::diagnostics diag;
         error_code ec;
 
         // Get a connection
@@ -282,15 +281,14 @@ public:
         if (ec)
             return error_with_message{ec, diag.server_message()};
 
-        // Prepare a statement
-        constexpr std::string_view stmt_sql = R"SQL(SELECT id, username  FROM users WHERE id = ?)SQL";
-        auto stmt = conn->async_prepare_statement(stmt_sql, diag, yield[ec]);
-        if (ec)
-            return error_with_message{ec, diag.server_message()};
-
-        // Execute it
-        boost::mysql::static_results<user> result;
-        conn->async_execute(stmt.bind(user_id), result, diag, yield[ec]);
+        // Run the query
+        mysql::static_results<user> result;
+        conn->async_execute(
+            mysql::with_params("SELECT id, username  FROM users WHERE id = {}", user_id),
+            result,
+            diag,
+            yield[ec]
+        );
         if (ec)
             return error_with_message{ec, diag.server_message()};
 
@@ -307,10 +305,12 @@ public:
         boost::asio::yield_context yield
     ) final override
     {
+        // Check that we have one user ID, at least.
+        // Otherwise, the generated query may not be valid.
         if (user_ids.empty())
             return {};
 
-        boost::mysql::diagnostics diag;
+        mysql::diagnostics diag;
         error_code ec;
 
         // Get a connection
@@ -318,20 +318,17 @@ public:
         if (ec)
             return error_with_message{ec, diag.server_message()};
 
-        // Compose the SQL statement.
-        // WARNING: this is safe only because user IDs are numeric, and not strings.
-        // DO NOT USE THIS APPROACH WITH STRINGS, or you will get SQL injections.
-        // See https://github.com/boostorg/mysql/issues/69
-        std::ostringstream oss;
-        oss << "SELECT id, username FROM users WHERE id IN (" << user_ids[0];
-        for (auto it = std::next(user_ids.begin()); it != user_ids.end(); ++it)
-            oss << "," << *it;
-        oss << ')';
-
-        // Execute it
+        // Execute the query.
+        // We can safely do this because we checked that user_ids is not empty.
+        // Otherwise, the client-side generated query wouldn't be valid.
         using row_t = std::tuple<std::int64_t, std::string>;
-        boost::mysql::static_results<row_t> result;
-        conn->async_execute(oss.str(), result, diag, yield[ec]);
+        mysql::static_results<row_t> result;
+        conn->async_execute(
+            mysql::with_params("SELECT id, username FROM users WHERE id IN ({})", user_ids),
+            result,
+            diag,
+            yield[ec]
+        );
         if (ec)
             return error_with_message{ec, diag.server_message()};
 
