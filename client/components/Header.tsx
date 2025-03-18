9e338ed6d03d4df3d220cf62faf9d5d538ac414f
Ruben Perez <rubenperez038@gmail.com>
2025-03-18 16:13:41 +0100

    Header basics

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <div className="flex m-3">
       <Image src={boostLogo} height={60} alt="Boost logo"></Image>
-      <div className="flex-1 flex justify-end align-middle">
+      <div className="flex-1 flex justify-end align-middle invisible md:visible">
         {links.map(({ text, href }) => (
           <div key={href} className="flex flex-col justify-center pr-12 pl-12">
             <a className="no-underline text-2xl text-black" href={href}>
