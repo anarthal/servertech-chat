e14f9f114210d9b2ccc7f5fd42198901d45448ec
Ruben Perez <rubenperez038@gmail.com>
2025-03-18 16:13:50 +0100

    Index basics

--- client/components/FormCard.tsx
+++ client/components/FormCard.tsx
@@ -5,11 +5,8 @@ export default function FormCard({
   title,
 }: PropsWithChildren<{ title: string }>) {
   return (
-    <div
-      className="bg-white rounded-2xl p-7 flex flex-col"
-      style={{ minWidth: "50%" }}
-    >
-      <p className="text-center text-4xl p-3 m-0">{title}</p>
+    <div className="bg-white rounded-2xl p-7 flex flex-col min-w-1/2">
+      <p className="text-center text-3xl md:text-4xl p-3 m-0">{title}</p>
       <div className="flex justify-center">
         <div
           className="pt-8 pr-5 pl-5 pb-3 flex-1 flex"
