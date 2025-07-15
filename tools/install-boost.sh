877af5d61caea06f66ec0b32ae2c5b5045642c6d
Ruben Perez <rubenperez038@gmail.com>
2025-07-15 11:34:27 +0200

    Use the Boost 1.89 beta

--- tools/install-boost.sh
+++ tools/install-boost.sh
@@ -2,9 +2,9 @@
 # Get the source code
 mkdir ~/boost-src
 cd ~/boost-src
-wget -q https://archives.boost.io/release/1.87.0/source/boost_1_87_0.tar.gz
-tar -xf boost_1_87_0.tar.gz
-cd boost_1_87_0
+wget -q https://archives.boost.io/beta/1.89.0.beta1/source/boost_1_89_0_b1_rc1.tar.gz
+tar -xf boost_1_89_0.tar.gz
+cd boost_1_89_0
 
 # Build and install. Make sure you've got write access to /opt/boost,
 # otherwise change the --prefix argument
